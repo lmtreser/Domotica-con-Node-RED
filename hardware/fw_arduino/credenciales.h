1c8a648216a5e9adfacaabf4723c71f2654b4a48
lmtreser <lmtreser@gmail.com>
2024-02-05 17:22:16 -0300

    update fw arduino

--- hardware/fw_arduino/credenciales.h
+++ hardware/fw_arduino/credenciales.h
@@ -1,13 +1,13 @@
 //Credenciales de la red WiFi
-const char* ssid = "ssid";
-const char* password = "password";
+const char* ssid = "wifi_ssid";
+const char* password = "wifi_password";
 
 //Credenciales del broker MQTT
-const char* mqtt_server = "test.mosquitto.org";
-const char* mqtt_id = "domotica23";
-//const char* mqtt_user = "user";
-//const char* mqtt_pass = "password";
-const char* mqtt_topic_1 = "home99/sensor1";
-const char* mqtt_topic_2 = "home99/sensor2";
-const char* mqtt_topic_3 = "home99/actuador1";
+const char* mqtt_server = "mqtt_broker_host";
+const char* mqtt_id = "mqtt_broker_id";
+//const char* mqtt_user = "mqtt_broker_username";
+//const char* mqtt_pass = "mqtt_broker_password";
+const char* mqtt_topic_1 = "topic/sensor1";
+const char* mqtt_topic_2 = "topic/sensor2";
+const char* mqtt_topic_3 = "topic/actuador1";
 const int mqtt_port = 1883;
